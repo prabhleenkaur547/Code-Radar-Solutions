1f8a2a53d2b780fd57e3b7cc5c852caf825bf492
prabhleenkaur547 <kaurprabhleen714@gmail.com>
2025-02-16 09:20:24 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -2,8 +2,12 @@
 
 int main() {
     float num;
+    printf("enter float");
     scanf("%f",&num);
     printf("num is: ",num);
  
     return 0;
+     printf("enter radius");
+ scanf("%f",&radius);
+ printf("area is: %f",3.14*radius*radius);
 }
